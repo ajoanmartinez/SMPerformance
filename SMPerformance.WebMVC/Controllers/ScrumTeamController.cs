2ed18413f815e17c922f53c67f6e895af95910b8
Amy Martinez <ajmartinez0712@gmail.com>
2021-04-15 19:49:04 -0400

    Add TeamListItem and Team Index view

---
+++ SMPerformance.WebMVC/Controllers/ScrumTeamController.cs
@@ -0,0 +1,20 @@
+﻿using SMPerformance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SMPerformance.WebMVC.Controllers
+{
+    [Authorize]
+    public class ScrumTeamController : Controller
+    {
+        // GET: ScrumTeam
+        public ActionResult Index()
+        {
+            var model = new ScrumTeamListItem[0];
+            return View(model);
+        }
+    }
+}
