14755afa178f78e26fcb32b2a4abd473feec473c
Amy Martinez <ajmartinez0712@gmail.com>
2021-04-19 18:52:10 -0400

    Add ScrumMaster Controller and ScrumMaster Index view

---
+++ SMPerformance.WebMVC/Controllers/ScrumMasterController.cs
@@ -0,0 +1,20 @@
+﻿using SMPerformance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SMPerformance.WebMVC.Controllers
+{
+    [Authorize]
+    public class ScrumMasterController : Controller
+    {
+        // GET: ScrumMaster
+        public ActionResult Index()
+        {
+            var model = new ScrumMasterListItem[0];
+            return View(model);
+        }
+    }
+}
