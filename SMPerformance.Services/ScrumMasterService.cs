943901acdfd0c1d6b7137855b09cf99a5ea93de1
Amy Martinez <ajmartinez0712@gmail.com>
2021-04-20 18:09:14 -0400

    Add ScrumMaster Service

---
+++ SMPerformance.Services/ScrumMasterService.cs
@@ -0,0 +1,58 @@
+﻿using SMPerformance.Data;
+using SMPerformance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMPerformance.Services
+{
+    public class ScrumMasterService
+    {
+        private readonly Guid _userId;
+
+        public ScrumMasterService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateScrumMaster(ScrumMasterCreate model)
+        {
+            var entity =
+                new ScrumMaster()
+                {
+                    OwnerId = _userId,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.ScrumMasters.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<ScrumMasterListItem> GetScrumMasters()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .ScrumMasters
+                        .Where(e => e.OwnerId == _userId)
+                        .Select(
+                              e =>
+                                new ScrumMasterListItem
+                                {
+                                    ScrumMasterId = e.ScrumMasterId,
+                                    FirstName = e.FirstName,
+                                    LastName = e.LastName
+                                }
+                        );
+                return query.ToArray();
+            }
+        }
+    }
+}
