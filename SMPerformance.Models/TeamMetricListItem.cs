ce26189be46c9905ae22023d876a693bc3e9ca06
Amy Martinez <ajmartinez0712@gmail.com>
2021-04-25 11:04:56 -0400

    TeamMetricListItem model and view

---
+++ SMPerformance.Models/TeamMetricListItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMPerformance.Models
+{
+    public enum Quarter
+    {
+        Q1, Q2, Q3, Q4
+    }
+
+    public enum PerformanceRating
+    {
+        NotRated, NeedsImprovement, BelowAverage, Average, AboveAverage, ExceedsExpectations
+    }
+    public class TeamMetricListItem
+    {
+        [Display(Name = "Evaluation Id")]
+        public int EvalId { get; set; }
+
+        [Display(Name = "Scrum Team")]
+        public int Team { get; set; }
+
+        [Display(Name = "Scrum Master")]
+        public int ScrumMaster { get; set; }
+
+        [Display(Name = "Fiscal Year")]
+        public int Fiscalyear { get; set; }
+
+        [Display(Name = "Fiscal Quarter")]
+        public Quarter FiscalQuarter { get; set; }
+
+        [Display(Name = "Performance Rating")]
+        public PerformanceRating RatingOfPerformance { get; set; }
+    }
+}
---
+++ SMPerformance.WebMVC/Controllers/TeamMetricController.cs
@@ -0,0 +1,20 @@
+﻿using SMPerformance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SMPerformance.WebMVC.Controllers
+{
+    [Authorize]
+    public class TeamMetricController : Controller
+    {
+        // GET: TeamMetric
+        public ActionResult Index()
+        {
+            var model = new TeamMetricListItem[0];
+            return View(model);
+        }
+    }
+}
